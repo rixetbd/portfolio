e63dadecaffb5c579f3266cc1c7dcec3b4ac4e82
Muhammad Rabiul <72306656+RixetBD@users.noreply.github.com>
2021-03-17 00:34:34 +0600

    Add files via upload

---
+++ js/mr.script.js
@@ -0,0 +1,37 @@
+// Preloader Effect
+$(window).on('load', function () { // makes sure the whole site is loaded 
+    $('#status').fadeOut(); // will first fade out the loading animation 
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    $('body').delay(350).css({
+        'overflow': 'visible'
+    });
+})
+// Preloader Effect
+
+// Colorfull Scroll Bar Effect
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop(),
+        dh = $(document).height(),
+        wh = $(window).height();
+    scrollPercent = (scroll / (dh - wh)) * 100;
+    $('#progressbar').css('height', scrollPercent + '%');
+})
+// Colorfull Scroll Bar Effect
+
+// Clock
+const deg = 6; //360deg / 60(min||sec) => 6
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30; //360deg / 12 hour => 30
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+});
+// Clock
